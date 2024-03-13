ad95d100edce61519a8b3384aecf3533b34b29db
Nurmuhammad <mamajonov.dev@gmail.com>
2024-03-13 10:41:20 +0500

    first second

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@
 # env.read_env()
 
 
-BOT_TOKEN = "5894242634:AAGEakN8wZM1Mf6n6dr61RrcAjRaX4jj3Ls"  # Забираем значение типа str
+BOT_TOKEN = "6650778355:AAEWNMh_WyheoiSgXbUMje5TJ7b1Qmi7MI8"  # Забираем значение типа str
 ADMINS = [659237008,6230606860] # Тут у нас будет список из админов
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
 CHANNELS = '@my_designes'
