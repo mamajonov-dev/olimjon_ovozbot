5f6600c4119d6fc067ac07aeb3b9ad59632cef57
Nurmuhammad <mamajonov.dev@gmail.com>
2024-03-14 16:22:17 +0500

    change channel name to bagdodim_news

--- data/config.py
+++ data/config.py
@@ -8,4 +8,4 @@
 BOT_TOKEN = "6650778355:AAEWNMh_WyheoiSgXbUMje5TJ7b1Qmi7MI8"  # Забираем значение типа str
 ADMINS = [659237008,6230606860] # Тут у нас будет список из админов
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
-CHANNELS = '@my_designes'
+CHANNELS = '@bagdodim_news'
