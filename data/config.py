3cfd624c2a774642758e2146ad1efeb65e92d9d6
Nurmuhammad <91172118+mamajonov-dev@users.noreply.github.com>
2025-03-17 14:57:31 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -8,7 +8,7 @@
 # BOT_TOKEN = "6114280839:AAESlIlPS-bF9IYKi0vWNeNt4bBlH56tEuA"# Забираем значение типа str
 BOT_TOKEN = "6650778355:AAHrVGEGna3cd3gOhO8xRVxjs9ly03yQ9_o"  # asosiy
 # ADMINS = [659237008] # Тут у нас будет список из админов
-ADMINS = [659237008,975763554] # Asosiy
+ADMINS = [659237008,130167473] # Asosiy
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
 # CHANNELS = '@bagdaditacademy'
 # CHANNELS = '@mydesignesaa'
