47d79a41b13337af72089782b413df9385ece545
Nurmuhammad <91172118+mamajonov-dev@users.noreply.github.com>
2025-08-07 18:05:55 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,13 +5,15 @@
 # env.read_env()
 
 
-BOT_TOKEN = "6114280839:AAH70aKIbRVqrsw6i-Pt7HiIu0w-b5lQSr4"# Забираем значение типа str
-# BOT_TOKEN = "6650778355:AAHrVGEGna3cd3gOhO8xRVxjs9ly03yQ9_o"  # asosiy
-ADMINS = [659237008] # Тут у нас будет список из админов
-# ADMINS = [659237008,130167473] # Asosiy
+# BOT_TOKEN = "6114280839:AAH70aKIbRVqrsw6i-Pt7HiIu0w-b5lQSr4"# Забираем значение типа str
+# BOT_TOKEN = "6650778355:AAHrVGEGna3cd3gOhO8xRVxjs9ly03yQ9_o"  # asosiy olimjon voz bot
+BOT_TOKEN = "8243052825:AAHvTtEyt7Hmn5rC8ccrIatZ-8rPQWSEUsg"  # bagdodimnewsbot
+# ADMINS = [659237008] # Тут у нас будет список из админов
+ADMINS = [659237008, 130167473] # Asosiy
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
-CHANNELS = '@bagdaditacademy'
+# CHANNELS = '@bagdaditacademy'
 # CHANNELS = '@mydesignesaa'
-# CHANNELS = '@bagdodim_news'   #asosiy
+# CHANNELS = '@@bagdodimnewsbot'   # olimjon ovozbot
+CHANNELS = '@@bagdodimnewsbot'   #asosiy
 NUMBERCHANNELS = '@bagdadgetnumbers'
 TESTNUMBERCHANNELS = '@mydesignesaa'
