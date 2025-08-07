6b5906eef4c01eb3b76e6941dea4bf7730bac923
Nurmuhammad <91172118+mamajonov-dev@users.noreply.github.com>
2025-08-07 17:22:34 +0500

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def createposttable(postname):
     postname = f'{postname}'
     database = sqlite3.connect('database.sqlite')
     cursor = database.cursor()
-    cursor.execute(f'''CREATE TABLE IF NOT EXISTS "{postname}" (
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS {postname} (
     button_id INTEGER PRIMARY KEY AUTOINCREMENT,
     button_name VARCHAR,
     button_number INTEGER DEFAULT 0,
