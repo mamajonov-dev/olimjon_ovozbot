e66bcc78fa4041fad301a1e1657a829563da2a81
Nurmuhammad <91172118+mamajonov-dev@users.noreply.github.com>
2025-08-07 17:25:43 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,13 +5,13 @@
 # env.read_env()
 
 
-# BOT_TOKEN = "6114280839:AAESlIlPS-bF9IYKi0vWNeNt4bBlH56tEuA"# Забираем значение типа str
-BOT_TOKEN = "6650778355:AAHrVGEGna3cd3gOhO8xRVxjs9ly03yQ9_o"  # asosiy
-# ADMINS = [659237008] # Тут у нас будет список из админов
-ADMINS = [659237008,130167473] # Asosiy
+BOT_TOKEN = "6279378107:AAHE5LuybVlzWnx-VJanABIvqyM-HYTAr5s"# Забираем значение типа str
+# BOT_TOKEN = "6650778355:AAHrVGEGna3cd3gOhO8xRVxjs9ly03yQ9_o"  # asosiy
+ADMINS = [659237008] # Тут у нас будет список из админов
+# ADMINS = [659237008,130167473] # Asosiy
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
-# CHANNELS = '@bagdaditacademy'
+CHANNELS = '@bagdaditacademy'
 # CHANNELS = '@mydesignesaa'
-CHANNELS = '@bagdodim_news'   #asosiy
+# CHANNELS = '@bagdodim_news'   #asosiy
 NUMBERCHANNELS = '@bagdadgetnumbers'
 TESTNUMBERCHANNELS = '@mydesignesaa'
